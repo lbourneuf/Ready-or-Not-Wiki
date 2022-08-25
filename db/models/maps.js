7a410a4e86d02d2944c4680048108efbfdbbf416
Luke Bourneuf <lukebourneuf@gmail.com>
2022-08-25 12:00:14 -0400

    feat(created table definitions) created definitions and started to create all the maps

---
+++ db/models/maps.js
@@ -0,0 +1,41 @@
+// grab our db client connection to use with our adapters
+const client = require("../client");
+
+async function createMaps({ name, description, missionsAvailable, imageUrl }) {
+  try {
+    const {
+      rows: [map],
+    } = await client.query(
+      `
+      INSERT INTO maps(name, description, missions_available, image_url)
+      VALUES ($1, $2, $3, $4)
+      RETURNING *;
+    
+    `,
+      [name, description, missionsAvailable, imageUrl]
+    );
+    return map;
+  } catch (error) {
+    console.error("Error creating maps");
+    throw error;
+  }
+}
+
+async function getAllMaps() {
+  try {
+    const { rows } = await client.query(`
+      SELECT * 
+      FROM maps;
+    `);
+    return rows;
+  } catch (error) {
+    console.error("Trouble getting all maps");
+    throw error;
+  }
+}
+
+module.exports = {
+  // add your database adapter fns here
+  createMaps,
+  getAllMaps,
+};
